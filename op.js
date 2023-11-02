00293d6eb07420a50c0ba97958985704cda787f5
Daniel Stanley <137361216+tongxintiao@users.noreply.github.com>
2023-11-02 07:59:22 -0700

    Create op.js

---
+++ op.js
@@ -0,0 +1,9 @@
+.s-top-left-new .s-top-more .s-top-more-content a{width:60px;height:82px;float:left}
+.s-top-left-new .s-top-more .s-top-more-content img{width:42px;height:42px;margin-top:8px;margin-left:8px;margin-right:8px;border:1px solid rgba(0,0,0,.03);border-radius:8px;display:block}
+.s-top-left-new .s-top-more .s-top-more-content .s-top-more-title{width:60px;text-align:center;margin-top:3px}
+.s-top-left-new .s-top-more .s-top-more-content>a:hover{background-color:#f1f3fd;border-radius:8px}
+.s-top-left-new .s-top-more .s-top-more-content>a:hover .s-top-more-title{color:#315efb}
+.s-top-left-new .s-top-more .s-top-tomore{margin-top:0;width:212px;margin-left:8px;padding-top:5px;height:27px}
+.s-top-left-new .s-top-more .s-top-tomore:hover{background-color:#f1f3fd;border-radius:8px}
+.s-top-left-new .s-top-more .s-top-tomore:hover a{color:#315efb}
+.s-top-left-new .s-top-more-btn:hover .s-top-more{display:block}
